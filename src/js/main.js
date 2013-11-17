4a68eaabc60e47b851a16fd76df7ad6cc1c30876
Krzysztof Szczęsny <spooky9@poczta.onet.pl>
2013-11-17 23:41:27 +0100

    unit widget

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,49 @@
+var app = angular.module('app', ['ngRoute']);
 
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'HomeCtrl',
+            templateUrl: 'home.html'
+        })
+        .otherwise({
+            templateUrl: '404.html'
+        });
+}]);
+
+app.factory('data', ['$http', function($http) {
+    return $http.get('/data/index.json');
+}]);
+
+app.filter('colorize', function() {
+    return function(text) {
+        return angular.isNumber(text)
+            ? 'asd'
+            : text;
+    };
+})
+app.filter('tech', function() {
+    return function(text) {
+        var dict = {RULEUTL_Basic: 'T1'};
+        return dict[text] || 'T?';
+    };
+})
+
+app.directive('ngCard', [function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        templateUrl: 'card.html',
+        scope: {
+            item: '=ngContent'
+        }
+    }
+}]);
+
+app.controller('HomeCtrl', ['$scope', 'data', function($scope, data) {
+    $scope.factions = ['uef', 'cybran', 'aeon', 'seraphim'];
+
+    data.success(function(d) {
+        $scope.index = d;
+    });
+}]);
