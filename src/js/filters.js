c4ae2baaaa025493447b162c8fa0b57b7c777121
Krzysztof Szczęsny <spooky9@poczta.onet.pl>
2013-12-27 22:18:09 +0100

    reformat

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,15 +1,39 @@
 unitDb.filters = {
+    unsafe: ['$sce', function($sce) {
+        return function(val) {
+            return $sce.trustAsHtml(val);
+        }
+    }],
+
     pic: function() {
-            return function (item) {
-                if (!item) return;
-                return 'img/units/'+item.id+'.png';
-            }
-        },
+        return function (item) {
+            if (!item) return;
+            return 'img/units/'+item.id+'.png';
+        }
+    },
 
     icon: function() {
-            return function (item) {
-                if (!item) return;
-                return 'img/strategic/' + item.faction + '_' + item.strategicIcon +'.png';
-            }
+        return function (item) {
+            if (!item) return;
+            return 'img/strategic/' + item.faction + '_' + item.strategicIcon +'.png';
         }
+    },
+
+    flatten: function() {
+        return function(item) {
+            var separator = ', ';
+
+            if(angular.isArray(item)) {
+                return item.join(separator)
+            }
+
+            if (angular.isObject(item)) {
+                return _.map(item, function(v, k) {
+                    return k + (v === true ? '' :  ': ' + v)
+                }).join(separator)
+            }
+
+            return item;
+        }
+    }
 };
