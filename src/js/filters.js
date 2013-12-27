6e1073668da98bd0f47d695439468ca91d19ed3f
Krzysztof Szczęsny <spooky9@poczta.onet.pl>
2013-12-27 12:09:13 +0100

    name filter refactored into item method

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,11 +1,4 @@
 unitDb.filters = {
-    name: function() {
-            return function(item) {
-                if (!item) return;
-                return (item.name ? item.name + ': ' : '') + (item.tech == 'EXP' ? '' : item.tech + ' ') + item.description;
-            }
-        },
-
     pic: function() {
             return function (item) {
                 if (!item) return;
