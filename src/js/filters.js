e2938634f0e766b6c60f00a04194948ecab27776
Krzysztof Szczęsny <spooky9@poczta.onet.pl>
2014-02-05 12:03:16 +0100

    moved site-related filters to bottom

--- src/js/filters.js
+++ src/js/filters.js
@@ -5,20 +5,6 @@ unitDb.filters = {
         }
     }],
 
-    pic: function() {
-        return function (item) {
-            if (!item) return;
-            return 'img/units/'+item.id+'.png';
-        }
-    },
-
-    icon: function() {
-        return function (item) {
-            if (!item) return;
-            return 'img/strategic/' + item.faction + '_' + item.strategicIcon +'.png';
-        }
-    },
-
     flatten: function() {
         return function(item) {
             var separator = '<br/> ';
@@ -51,6 +37,20 @@ unitDb.filters = {
     round: function() {
         return function(item) {
             return Math.round(item);
+        }
+    },
+
+    pic: function() {
+        return function (item) {
+            if (!item) return;
+            return 'img/units/'+item.id+'.png';
+        }
+    },
+
+    icon: function() {
+        return function (item) {
+            if (!item) return;
+            return 'img/strategic/' + item.faction + '_' + item.strategicIcon +'.png';
         }
     }
 };
