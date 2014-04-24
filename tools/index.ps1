6138b7bc0cd24300fda3fb000eb6417bc91f3711
Krzysztof Szczęsny <spooky9@poczta.onet.pl>
2014-04-24 23:04:20 +0200

    extended powershell script

--- tools/index.ps1
+++ tools/index.ps1
@@ -1,6 +1,7 @@
 param (
-    [string]$unitDir = "C:\Temp\!faforever",
-    [string]$target = "C:\Temp\index.json"
+    [string]$target = "d:\code\personal\html\unitdb\src\data\index.json",
+    [string]$faUnitFile = "d:\games\steam\SteamApps\common\Supreme Commander Forged Alliance\gamedata\units.scd",
+    [string]$fafFile = "c:\ProgramData\FAForever\gamedata\faforever.nxt"
 )
 
 Function Create-UnitIndex {
@@ -8,7 +9,7 @@ Function Create-UnitIndex {
         [string]$unitDir
     )
 
-    $blueprints = Get-ChildItem "$unitDir\**\*.bp"
+    $blueprints = Get-ChildItem "$unitDir\**\*_unit.bp"
     $count = 1
     $total = $blueprints.Count
 
@@ -27,10 +28,31 @@ Function Create-UnitIndex {
     echo ']'
 }
 
-$json = Create-UnitIndex $unitDir
-$Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
-[System.IO.File]::WriteAllLines($target, $json, $Utf8NoBomEncoding)
+Function Run {
+    param (
+        [string]$target
+    )
 
+    $tempDir = "C:\Temp\!fafUnits"
 
-Write-Progress -Activity "Sorting"
-python sorter.py $target
+    Write-Progress -Activity "Cleaning"
+    If (Test-Path $tempDir) {
+        Remove-Item -Recurse -Force $tempDir
+    }
+
+    Write-Progress -Activity "Extracting Forged Alliance units"
+    7z x "$faUnitFile" -o"$tempDir"
+
+    Write-Progress -Activity "Extracting FAF units"
+    7z x "$fafFile" "units" -o"$tempDir" -y
+
+    Write-Progress -Activity "Creating unit index"
+    $json = Create-UnitIndex "$tempDir/units"
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+    [System.IO.File]::WriteAllLines($target, $json, $Utf8NoBomEncoding)
+
+    Write-Progress -Activity "Sorting"
+    python sorter.py $target
+}
+
+Run $target
