bb16e8b5fa45e90489622612e867ec54f387d25a
Krzysztof Szczęsny <spooky9@poczta.onet.pl>
2014-08-21 16:58:18 +0200

    dps changes

--- app/js/main.js
+++ app/js/main.js
@@ -51,6 +51,58 @@ var unitDb = (function() {
     };
 })();
 
+// dps fun
+unitDb.DpsCalculator = {
+    next : null,
+    canCalculate: function() { return false; },
+    _dps: function() { },
+    dps: function(w) {
+        if (this.canCalculate(w))
+            return this._dps(w);
+        else if (this.next && this.next.dps) {
+            return this.next.dps(w);
+        }
+    }
+};
+unitDb.DefaultDpsCalculator = angular.extend({}, unitDb.DpsCalculator, {
+    canCalculate: function() {
+        return true;
+    },
+    _dps: function(w) {
+        return (w.Damage * w.MuzzleSalvoSize) / (Math.round(10 / w.RateOfFire) / 10);
+    }
+});
+unitDb.BeamDpsCalculator = angular.extend({}, unitDb.DpsCalculator, {
+    next: unitDb.DefaultDpsCalculator,
+    canCalculate: function(w) {
+        return w.BeamLifetime;
+    },
+    _dps: function(w) {
+        return w.Damage * w.BeamLifetime * (w.BeamCollisionDelay || 1) * 10 / (Math.round(10 / w.RateOfFire) / 10);
+    }
+});
+unitDb.ContinousBeamDpsCalculator = angular.extend({}, unitDb.DpsCalculator, {
+    next: unitDb.BeamDpsCalculator,
+    canCalculate: function(w) {
+        return w.ContinuousBeam;
+    },
+    _dps: function(w) {
+        return (w.Damage * 10) / (w.BeamCollisionDelay === 0 ? 1 : 2);
+    }
+});
+unitDb.DoTDpsCalculator = angular.extend({}, unitDb.DpsCalculator, {
+    next: unitDb.ContinousBeamDpsCalculator,
+    canCalculate: function(w) {
+        return w.DoTPulses;
+    },
+    _dps: function(w) {
+        var initial = unitDb.DefaultDpsCalculator._dps(w);
+        return (initial + w.Damage * w.DoTPulses * w.MuzzleSalvoSize) /  (Math.round(10 / w.RateOfFire) / 10);
+    }
+});
+
+unitDb.dpsCalculator = unitDb.DoTDpsCalculator;
+
 // decorator to make the unit object a bit more usable
 unitDb.UnitDecorator = function(blueprint) {
     var classificationLookup = {
@@ -100,8 +152,7 @@ unitDb.UnitDecorator = function(blueprint) {
             return stats.shots + ' shot' + (stats.shots > 1 ? 's' : '') + ' / ' + ( stats.cycle === 1 ? '' : Math.round(stats.cycle * 10)/10 ) + ' sec';
         },
         getDps = function(weapon) {
-            var stats = weaponStats(weapon);
-            return (stats.damage * stats.shots) / stats.cycle;
+            return unitDb.dpsCalculator.dps(weapon);
         };
 
         var self = {
