960ef98b458a30f63f8e214750956eaefd25ee11
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-05-27 20:20:40 +0200

    Remove deprecated grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -303,11 +303,11 @@ module.exports = function (grunt) {
     },
 
     // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
+    // cdnify: {
+    //   dist: {
+    //     html: ['<%= yeoman.dist %>/*.html']
+    //   }
+    // },
 
     // Copies remaining files to places other tasks can use
     copy: {
@@ -456,7 +456,7 @@ module.exports = function (grunt) {
     'concat',
     'ngmin',
     'copy:dist',
-    'cdnify',
+    // 'cdnify',
     'cssmin',
     'uglify',
     'rev',
